b3a01596e9ccb610eda04b507f7b8f498633652a
Songqing Liu <sliu@west.com>
2015-01-22 10:41:07 -0600

    add schemas application

--- kraken/apps/clients/urls.py
+++ kraken/apps/clients/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls import patterns, url
 
-
 urlpatterns = patterns('kraken.apps.clients.views',
-
-
-)
+                       url(r'^clients/$', 'clients', name='clients'),
+                       )
