f21bfa74c690110564ccc733f7fada1ac0534ad5
Songqing Liu <sliu@west.com>
2015-02-05 11:06:24 -0600

    create schema_version if create client_schema

--- kraken/apps/schemas/urls.py
+++ kraken/apps/schemas/urls.py
@@ -10,10 +10,9 @@ urlpatterns = patterns('kraken.apps.schemas.views',
                        url(r'^clients/(?P<client_id>\d+)/schemas/create/$', 'create_schema', name='create_schema'),
                        url(r'^schemas/versions/create/$', 'create_version', name='create_version'),
                        url(r'^schemas/files/create/$', 'create_file', name='create_file'),
-                       url(r'^schemas/save/$', 'save_schema', name='save_schema'),
+                       url(r'^schemas/(?P<client_id>\d+)/save/$', 'save_schema', name='save_schema'),
                        url(r'^schemas/batch_files/$', 'batch_files', name='batch_files'),
 
-                       url(r'^client/schemas/new/$', views.client_schema_new, name='client_schema_new'),
                        url(r'^client/(?P<client_id>\d+)/schemas/$', views.client_schemas_list, name='client_schemas_list'),
 
                        url(r'^client/schema/versions/new/$', views.schema_version_new, name='schema_version_new'),
--- kraken/apps/schemas/views.py
+++ kraken/apps/schemas/views.py
@@ -7,6 +7,7 @@ from django.views.decorators.csrf import csrf_exempt
 from kraken.apps.core import messages
 from kraken.apps.core.models import Client, ClientSchema
 from kraken.apps.core.forms import ClientSchemaForm
+from kraken.apps.schemas.forms import SchemaVersionForm
 from kraken.apps.schemas.models import SchemaVersion, VersionBatch, SchemaColumn
 
 
@@ -28,6 +29,7 @@ def create_schema(request, client_id):
             'client': client,
             'state': 'create',
             'schema_form': ClientSchemaForm(),
+            'version_form': SchemaVersionForm({'delimiter': SchemaVersion.FIXED}),
             'client_schemas': client_schemas
         }
         return render(request, "schemas/schemas.html", context)
@@ -52,34 +54,47 @@ def create_version(request):
 
 
 @login_required
-def save_schema(request):
+def save_schema(request, client_id):
     if request.method == "POST":
         schema_form = ClientSchemaForm(request.POST)
+        version_form = SchemaVersionForm(request.POST)
+        #print version_form
         try:
             if schema_form.is_valid():
                 schema = schema_form.save()
-                messages.success(request, 'Schema \"{0}\" has been created.'.format(schema.name))
-                return redirect('core:home')
+                #messages.success(request, 'Schema \"{0}\" has been created.'.format(schema.name))
+                # Next step create version of schema
+                try:
+                    if version_form.is_valid():
+                        version = version_form.save()
+                        messages.success(request, 'Schema \"{0}\" and Version \"{1}\" have been created'.format(schema.name, version.identifier))
+                        return redirect('core:home')
+                    else:
+                        schema.delete()
+                        if version_form['identifier'].errors:
+                            errors_message = version_form['identifier'].errors
+                        else:
+                            errors_message = 'Version Name field is not a valid value'
+                        messages.danger(request, errors_message)
+                        return redirect('schemas:create_schema', client_id)
+                except Exception as e:
+                    messages.danger(request, e.message)
+                    return redirect('core:home')
             else:
                 if schema_form['name'].errors:
                     errors_message = schema_form['name'].errors
                 else:
-                    errors_message = 'No Client Foreign key provided.'
+                    errors_message = 'Client Name field is not a valid value'
                 messages.danger(request, errors_message)
-                return redirect('schemas:create_schema', 1)
+                return redirect('schemas:create_schema', client_id)
         except Exception as e:
             messages.danger(request, e.message)
             return redirect('core:home')
     return HttpResponseNotFound()
 
 
-
 def schemas(request):
     return render(request, 'schemas/schemas.html')
-
-
-def client_schema_new(request):
-    pass
 
 
 def client_schemas_list(request, client_id):
