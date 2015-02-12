340aaa60245a6361e37f8cf2b1d1a8262280df88
Eric Whitcomb <EWhitcomb@west.com>
2015-02-11 19:11:31 -0600

    Set up download file view.

--- kraken/apps/schemas/views.py
+++ kraken/apps/schemas/views.py
@@ -68,6 +68,16 @@ def edit_file(request, client_id, schema_id, version_id, file_id):
 
 
 @login_required
+def download_file(request, client_id, schema_id, version_id, file_id):
+    if request.method == "GET":
+        client = get_object_or_404(Client, pk=client_id)
+        schema = get_object_or_404(ClientSchema, pk=schema_id)
+        version = get_object_or_404(SchemaVersion, pk=version_id)
+        file = get_object_or_404(SchemaVersion, pk=version_id)
+        return render(request, "schemas/home.html")
+    return HttpResponseNotFound()
+
+@login_required
 def edit_version(request, client_id, schema_id, version_id):
     if request.method == "GET":
         client = get_object_or_404(Client, pk=client_id)
