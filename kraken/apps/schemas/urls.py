949e6a680059c8ae341f0238d7e561fcd724dfd0
Eric Whitcomb <EWhitcomb@west.com>
2015-02-17 13:58:24 -0600

    Fixed typo in schemas.urls.

--- kraken/apps/schemas/urls.py
+++ kraken/apps/schemas/urls.py
@@ -4,10 +4,10 @@ from kraken.apps.schemas import views
 
 urlpatterns = patterns('kraken.apps.schemas.views',
                        url(r'^clients/(?P<client_id>\d+)/schemas/create/$', 'create_schema', name='create_schema'),
-                       url(r'^clients/(?P<client_id>\d+)/schemas/save/$', 'save_schema', name='save_schema'),
                        url(r'^clients/(?P<client_id>\d+)/schemas/(?P<schema_id>\d+)/versions/create/$', 'create_version', name='create_version'),
                        url(r'^clients/(?P<client_id>\d+)/schemas/(?P<schema_id>\d+)/versions/(?P<version_id>\d+)/edit/$', 'edit_version', name='edit_version'),
                        url(r'^clients/(?P<client_id>\d+)/schemas/(?P<schema_id>\d+)/versions/(?P<version_id>\d+)/files/create/$', 'create_file', name='create_file'),
+                       url(r'^clients/(?P<client_id>\d+)/schemas/(?P<schema_id>\d+)/versions/(?P<version_id>\d+)/files/save/$', 'save_file', name='save_file'),
                        url(r'^clients/(?P<client_id>\d+)/schemas/(?P<schema_id>\d+)/versions/(?P<version_id>\d+)/files/(?P<file_id>\d+)/download/$', 'download_file', name='download_file'),
                        url(r'^clients/(?P<client_id>\d+)/schemas/(?P<schema_id>\d+)/versions/(?P<version_id>\d+)/files/(?P<file_id>\d+)/edit/$', 'edit_file', name='edit_file'),
 
