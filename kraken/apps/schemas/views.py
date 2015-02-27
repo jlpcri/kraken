5b00658ccfa70df389c2b845b7004978cfb7dcd8
Songqing Liu <sliu@west.com>
2015-02-27 14:17:01 -0600

    remove redundant code of 'save_file'

--- kraken/apps/schemas/views.py
+++ kraken/apps/schemas/views.py
@@ -53,6 +53,7 @@ def create_file(request, client_id, schema_id, version_id):
                 if file_form.is_valid():
                     file = file_form.save(commit=False)
                     file.schema_version = get_object_or_404(SchemaVersion, pk=version_id)
+                    file.contents = request.POST.get('textareaViewer', '')
                     file.save()
                     messages.success(request, 'File \"{0}\" has been created'.format(file.name))
                     return redirect('core:home')
@@ -316,37 +317,6 @@ def edit_version(request, client_id, schema_id, version_id):
                 messages.danger(request, e.message)
                 return redirect('schemas:edit_version', client_id, schema_id, version_id)
 
-    return HttpResponseNotFound()
-
-
-@login_required
-def save_file(request, client_id, schema_id, version_id):
-    if request.method == "POST":
-        if 'save_file' in request.POST:
-            file_form = VersionFileForm(request.POST)
-            try:
-                if file_form.is_valid():
-                    file = file_form.save(commit=False)
-                    file.schema_version = get_object_or_404(SchemaVersion, pk=version_id)
-                    file_contents = request.POST.get('textareaViewer', '')
-                    if not file_contents:
-                        messages.danger(request, 'No contents need saved to file')
-                        return redirect('schemas:create_file', client_id, schema_id, version_id)
-                    else:
-                        file.contents = file_contents
-                        file.save()
-                        messages.success(request, 'File \"{0}\" has been created'.format(file.name))
-                        return redirect('core:home')
-                else:
-                    if file_form['name'].errors:
-                        error_message = file_form['name'].errors
-                    else:
-                        error_message = 'Something went wrong'
-                    messages.danger(request, error_message)
-                    return redirect('schemas:create_file', client_id, schema_id, version_id)
-            except Exception as e:
-                messages.danger(request, e.message)
-                return redirect('core:home')
     return HttpResponseNotFound()
 
 
