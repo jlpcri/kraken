d5e02d39b1708cbdef7891da07b59b1ffe1b471d
Songqing Liu <sliu@west.com>
2015-03-02 16:19:56 -0600

    add more unit tests

--- kraken/apps/schemas/tests/test_schemas_files.py
+++ kraken/apps/schemas/tests/test_schemas_files.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.models import User
+from django.core.files.base import ContentFile
 from django.test import TestCase, Client
 from django.core.urlresolvers import reverse, resolve
 
-from kraken.apps.core.models import Client as KrakenClient, ClientSchema, SchemaVersion
-from kraken.apps.schemas.views import create_file
+from kraken.apps.core.models import Client as KrakenClient, ClientSchema, SchemaVersion, VersionFile
+from kraken.apps.schemas.views import create_file, download_file
 
 
-class TestVersionFiles(TestCase):
+class TestCreateVersionFiles(TestCase):
     def setUp(self):
         self.client = Client()
         self.kraken_client = KrakenClient.objects.create(name='Kraken Client')
@@ -70,5 +71,45 @@ class TestVersionFiles(TestCase):
         self.assertContains(response, 'File Name field is required')
 
 
+class TestDownloadVersionFiles(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.kraken_client = KrakenClient.objects.create(name='Kraken Client')
+        self.kraken_schema = ClientSchema.objects.create(name='Client Schema', client=self.kraken_client)
+        self.kraken_version = SchemaVersion.objects.create(identifier='New Identifier', client_schema=self.kraken_schema)
+        self.kraken_file = VersionFile.objects.create(name='File A', schema_version=self.kraken_version)
 
+        self.file_contents = {
+            'contents': 'aaaaaabbbbbb'
+        }
+        self.kraken_file.contents.save(self.kraken_file.name, ContentFile(self.file_contents['contents']))
 
+        self.url_download_file = reverse(
+            'schemas:download_file',
+            args=[self.kraken_client.id, self.kraken_schema.id, self.kraken_version.id, self.kraken_file.id]
+        )
+        self.user_account = {
+            'username': 'Test Username',
+            'password': 'Test Password',
+        }
+        self.user = User.objects.create_user(
+            username=self.user_account['username'],
+            password=self.user_account['password'],
+        )
+        self.client.login(
+            username=self.user_account['username'],
+            password=self.user_account['password']
+        )
+
+    def test_download_files_url_resolve_to_view(self):
+        found = resolve(self.url_download_file)
+        self.assertEqual(found.func, download_file)
+
+    def test_download_files_url_get_return_status_200(self):
+        response = self.client.get(self.url_download_file, follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_download_file_contains_file_contents(self):
+        response = self.client.get(self.url_download_file, follow=True)
+
+        self.assertContains(response, self.file_contents['contents'])
