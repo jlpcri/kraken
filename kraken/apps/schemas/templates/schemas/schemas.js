2e76ba327c35ee01faa35d9534cbf02aed78196e
Songqing Liu <sliu@west.com>
2015-03-06 15:06:58 -0600

    file contents save works now

--- kraken/apps/schemas/templates/schemas/schemas.js
+++ kraken/apps/schemas/templates/schemas/schemas.js
@@ -25,7 +25,7 @@ $("button[name='save_file']").on('click', function () {
     } else if ( !$("#id_name").val() ){   //check file name empty
         showErrMsg('No input of file name');
         return false;
-    } else if (file_names.indexOf($('#id_name').val()) > -1 ) {
+    } else if ($.inArray($('#id_name').val(), file_names) > -1) {
         showErrMsg('File Name is duplicated');
         return false;
     }
