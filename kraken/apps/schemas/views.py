4ca0859b866178567225b912dd641a129353b0a5
Songqing Liu <sliu@west.com>
2015-03-11 15:03:03 -0500

    fix KRAK-58

--- kraken/apps/schemas/views.py
+++ kraken/apps/schemas/views.py
@@ -252,6 +252,7 @@ def edit_file(request, client_id, schema_id, version_id, file_id):
             'fields': fields,
             'field_number': len(fields),
             'file_columns': [item[1] for item in FileColumn.GENERATOR_CHOICES],
+            'number_columns': [item[1] for item in FileColumn.NUMBER_GENERATOR_CHOICES],
             'column_parameters': column_parameters,
             'state': 'save',
             'file_form': file_form,
