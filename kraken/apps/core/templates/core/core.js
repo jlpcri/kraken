07b7b436cf22fc68c336767cf23839d5079defa2
Songqing Liu <sliu@west.com>
2015-03-23 10:46:07 -0500

    js session expired set to 8 hours

--- kraken/apps/core/templates/core/core.js
+++ kraken/apps/core/templates/core/core.js
@@ -1,6 +1,30 @@
-/**
- * Created by sliu on 2/4/15.
- */
+// Session Timeout Redirect to Login
+var session_expired,
+    session_lastActivity,
+    session_now,
+    down,
+    session_check_interval = 30* 60 * 1000;
+
+
+function InitSessionTimer(){
+    session_expired = 8 * 60 * 60 * 1000;  //Actual timeout 8 hours in milliseconds
+    session_lastActivity = new Date().getTime();
+    CheckSessionStatus();
+}
+
+InitSessionTimer();
+
+function CheckSessionStatus(){
+    // Check for session warning
+    session_now = new Date().getTime();
+    if (session_now > session_lastActivity + session_expired) {
+        window.location.href = '/kraken/signout/';
+    } else {
+        down = setTimeout('CheckSessionStatus();', session_check_interval);
+    }
+}
+
+
 
 // Client
 $("#modal_create_client_form").submit(function(event) {
